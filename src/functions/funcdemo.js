67548e62a366716bcf18c3934220dd7c86075921
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-01-23 08:00:12 +0700

    Azure function cicd with git action

--- src/functions/funcdemo.js
+++ src/functions/funcdemo.js
@@ -6,7 +6,7 @@ app.http('funcdemo', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'I am almost succeed with Azure Functions! by applying CI/CD with gitHub Actions';
+        const name = request.query.get('name') || await request.text() || 'I am almost success with Azure Functions! By applying CI/CD with gitHub Actions';
 
         return { body: `Hello, ${name}!` };
     }
